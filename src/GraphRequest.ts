1343b858ac040e416089f420d484d99f9924b8fd
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2018-09-28 12:08:34 -0300

    feat(polyfill): do not polyfill
    Do not polyfill Promise, neither fetch
    Let users bring their own implementations
    
    fix #113

--- src/GraphRequest.ts
+++ src/GraphRequest.ts
@@ -1,6 +1,3 @@
-import { Promise } from 'es6-promise'
-import 'isomorphic-fetch';
-
 import { Options, URLComponents, oDataQueryNames, GraphRequestCallback, PACKAGE_VERSION, DefaultRequestHeaders, FetchOptions } from "./common"
 import { ResponseHandler } from "./ResponseHandler"
 import { RequestMethod } from './RequestMethod';
