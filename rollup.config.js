4010b7c673496c47b3e00b1e382faa4527028a65
Muthurathinam <muthurathinam@zoho.com>
2019-04-15 13:16:29 +0530

    removed unwanted entry from rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import { terser } from "rollup-plugin-terser";
 
 const config = [
 	{
-		external: ["es6-promise", "isomorphic-fetch"],
 		input: ["lib/es/browser/index.js"],
 		output: {
 			file: "lib/graph-es-sdk.js",
