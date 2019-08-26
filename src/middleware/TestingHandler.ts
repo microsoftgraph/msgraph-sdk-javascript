921751a39f5cc9a5becbd2c43ad7da5d346f1c01
krototype <abhinav.0543@gmail.com>
2019-08-26 21:09:20 +0530

    changed the structure of testing Handler

--- src/middleware/TestingHandler.ts
+++ src/middleware/TestingHandler.ts
@@ -23,161 +23,12 @@ export class TestingHandler implements Middleware {
 	 * @private
 	 * A member holding options to customize the handler behavior
 	 */
+
 	private options: TestingHandlerOptions;
 
-	private responseMap: Map<string, Map<string, string>> = new Map([
-		[
-			"2xx",
-			new Map([
-				[
-					"responseHeader",
-					`{
-					"Cache-Control" : "",
-					"Transfer-Encoding" : "",
-					"Content-Type" : "",
-					"Content-Encoding" : "",
-					"Vary" : "",
-					"request-id" : "",
-					"client-request-id" : "",
-					"x-ms-ags-diagnostic" : "",
-					"OData-Version" : 4.0,
-					"Duration" : 103.8879,
-					"Strict-Transport-Security" : "",
-					"Date" : ""
-					}`,
-				],
-				["responseBody", "{}"],
-			]),
-		],
-		[
-			"3xx",
-			new Map([
-				[
-					"responseHeader",
-					`{
-					"Cache-Control" : "",
-					"Transfer-Encoding" : "",
-					"Content-Type" : "",
-					"Content-Encoding" : "",
-					"Vary" : "",
-					"request-id" : "",
-					"client-request-id" : "",
-					"x-ms-ags-diagnostic" : "",
-					"OData-Version" : 4.0,
-					"Duration" : 103.8879,
-					"Strict-Transport-Security" : "",
-					"Date" : ""
-					}`,
-				],
+	public statusCode: number;
 
-				["responseBody", "{}"],
-			]),
-		],
-		[
-			"4xx",
-			new Map([
-				[
-					"responseHeader",
-					`{
-					"Cache-Control" : "",
-					"Transfer-Encoding" : "",
-					"Content-Type" : "",
-					"request-id" : "",
-					"client-request-id" : "",
-					"x-ms-ags-diagnostic" : "",
-					"Duration" : 103.8879,
-					"Strict-Transport-Security" : "",
-					"Date" : ""
-				}`,
-				],
-
-				[
-					"responseBody",
-					`{
-					"error": {
-						"code": "BadRequest",
-						"message": "",
-						"innerError": {
-							"request-id": "c1e7bf3f-74e9-4ba7-a0f6-835e065f8f45",
-							"date": "2019-08-20T10:41:33"
-						}
-					}
-				}`,
-				],
-			]),
-		],
-		[
-			"429",
-			new Map([
-				[
-					"responseHeader",
-					`{
-					"Cache-Control" : "",
-					"Transfer-Encoding" : "",
-					"Content-Type" : "",
-					"request-id" : "",
-					"client-request-id" : "",
-					"x-ms-ags-diagnostic" : "",
-					"Duration" : 103.8879,
-					"Strict-Transport-Security" : "",
-					"timeout": 300,
-					"Date" : ""
-				}`,
-				],
-
-				[
-					"responseBody",
-					`{
-					"error": {
-						"code": "BadRequest",
-						"message": "",
-						"innerError": {
-							"request-id": "c1e7bf3f-74e9-4ba7-a0f6-835e065f8f45",
-							"date": "2019-08-20T10:41:33"
-						}
-					}
-				}`,
-				],
-			]),
-		],
-		[
-			"5xx",
-			new Map([
-				[
-					"responseHeader",
-					`{
-					"Cache-Control" : "",
-					"Transfer-Encoding" : "",
-					"Content-Type" : "",
-					"request-id" : "",
-					"client-request-id" : "",
-					"x-ms-ags-diagnostic" : "",
-					"Duration" : 103.8879,
-					"Strict-Transport-Security" : "",
-					"Date" : ""
-				}`,
-				],
-
-				[
-					"responseBody",
-					`{
-					"error": {
-						"code": "BadRequest",
-						"message": "",
-						"innerError": {
-							"request-id": "c1e7bf3f-74e9-4ba7-a0f6-835e065f8f45",
-							"date": "2019-08-20T10:41:33"
-						}
-					}
-				}`,
-				],
-			]),
-		],
-	]);
-
-	// private map2xx: Map<string, string> = ;
-
-	// private map3xx: Map<string, string> = ;
+	public statusMessage: string;
 
 	public constructor(options: TestingHandlerOptions = new TestingHandlerOptions()) {
 		// console.log("declaring through testing Handler");
@@ -202,35 +53,6 @@ export class TestingHandler implements Middleware {
 		return options;
 	}
 
-	private async createResponse(context: Context): Promise<any> {
-		try {
-			const statusCode: number = this.options.statusCode;
-			const statusMessage: string = this.options.statusText;
-			let statusCodeKey: string;
-			let responseBody: string;
-			let responseHeader: string;
-
-			// console.log(statusCode, "statusCode");
-			// console.log(context.options.method);
-
-			statusCodeKey = statusCode === 429 ? "429" : `${Math.floor(statusCode / 100)}xx`;
-			responseHeader = this.responseMap.get(statusCodeKey).get("responseHeader");
-			responseBody = this.responseMap.get(statusCodeKey).get("responseBody");
-
-			// console.log(responseHeader);
-			// console.log(JSON.parse(responseBody));
-
-			// responseBody = this.responseMap[statusCodeKey].responseBody;
-
-			const init = { url: context.request as string, status: statusCode, statusText: statusMessage, header: JSON.parse(responseHeader) };
-			const response = new Response(JSON.parse(responseBody), init);
-			// console.log(response);
-			return response;
-		} catch (error) {
-			throw error;
-		}
-	}
-
 	/**
 	 * @public
 	 * @async
@@ -242,10 +64,9 @@ export class TestingHandler implements Middleware {
 		try {
 			// write the things to be executed in testing Handler
 			// Have to create a map for the purpose of Headers and body
-			this.options = this.getOptions(context);
+			this.options = await this.getOptions(context);
 			// console.log(context.options.method);
-			await this.options.sendMethod(context.options.method as string);
-			context.response = await this.createResponse(context);
+			context.response = await this.options.sendMethod(context.options.method as string, context.request as string);
 			return;
 		} catch (error) {
 			throw error;
--- src/middleware/options/TestingHandlerOptions.ts
+++ src/middleware/options/TestingHandlerOptions.ts
@@ -18,6 +18,158 @@ import { TestingStrategy } from "./TestingStrategy";
 export class TestingHandlerOptions implements MiddlewareOptions {
 	private apiMethod: string;
 
+	private apiURL: string;
+
+	private responseMap: Map<string, Map<string, string>> = new Map([
+		[
+			"2xx",
+			new Map([
+				[
+					"responseHeader",
+					`{
+					"Cache-Control" : "",
+					"Transfer-Encoding" : "",
+					"Content-Type" : "",
+					"Content-Encoding" : "",
+					"Vary" : "",
+					"request-id" : "",
+					"client-request-id" : "",
+					"x-ms-ags-diagnostic" : "",
+					"OData-Version" : 4.0,
+					"Duration" : 103.8879,
+					"Strict-Transport-Security" : "",
+					"Date" : ""
+					}`,
+				],
+				["responseBody", "{}"],
+			]),
+		],
+		[
+			"3xx",
+			new Map([
+				[
+					"responseHeader",
+					`{
+					"Cache-Control" : "",
+					"Transfer-Encoding" : "",
+					"Content-Type" : "",
+					"Content-Encoding" : "",
+					"Vary" : "",
+					"request-id" : "",
+					"client-request-id" : "",
+					"x-ms-ags-diagnostic" : "",
+					"OData-Version" : 4.0,
+					"Duration" : 103.8879,
+					"Strict-Transport-Security" : "",
+					"Date" : ""
+					}`,
+				],
+
+				["responseBody", "{}"],
+			]),
+		],
+		[
+			"4xx",
+			new Map([
+				[
+					"responseHeader",
+					`{
+					"Cache-Control" : "",
+					"Transfer-Encoding" : "",
+					"Content-Type" : "",
+					"request-id" : "",
+					"client-request-id" : "",
+					"x-ms-ags-diagnostic" : "",
+					"Duration" : 103.8879,
+					"Strict-Transport-Security" : "",
+					"Date" : ""
+				}`,
+				],
+
+				[
+					"responseBody",
+					`{
+					"error": {
+						"code": "BadRequest",
+						"message": "",
+						"innerError": {
+							"request-id": "c1e7bf3f-74e9-4ba7-a0f6-835e065f8f45",
+							"date": "2019-08-20T10:41:33"
+						}
+					}
+				}`,
+				],
+			]),
+		],
+		[
+			"429",
+			new Map([
+				[
+					"responseHeader",
+					`{
+					"Cache-Control" : "",
+					"Transfer-Encoding" : "",
+					"Content-Type" : "",
+					"request-id" : "",
+					"client-request-id" : "",
+					"x-ms-ags-diagnostic" : "",
+					"Duration" : 103.8879,
+					"Strict-Transport-Security" : "",
+					"timeout": 300,
+					"Date" : ""
+				}`,
+				],
+
+				[
+					"responseBody",
+					`{
+					"error": {
+						"code": "BadRequest",
+						"message": "",
+						"innerError": {
+							"request-id": "c1e7bf3f-74e9-4ba7-a0f6-835e065f8f45",
+							"date": "2019-08-20T10:41:33"
+						}
+					}
+				}`,
+				],
+			]),
+		],
+		[
+			"5xx",
+			new Map([
+				[
+					"responseHeader",
+					`{
+					"Cache-Control" : "",
+					"Transfer-Encoding" : "",
+					"Content-Type" : "",
+					"request-id" : "",
+					"client-request-id" : "",
+					"x-ms-ags-diagnostic" : "",
+					"Duration" : 103.8879,
+					"Strict-Transport-Security" : "",
+					"Date" : ""
+				}`,
+				],
+
+				[
+					"responseBody",
+					`{
+					"error": {
+						"code": "BadRequest",
+						"message": "",
+						"innerError": {
+							"request-id": "c1e7bf3f-74e9-4ba7-a0f6-835e065f8f45",
+							"date": "2019-08-20T10:41:33"
+						}
+					}
+				}`,
+				],
+			]),
+		],
+	]);
+
 	public testingStrategy: TestingStrategy;
 
 	public statusCode: number;
@@ -42,26 +194,60 @@ export class TestingHandlerOptions implements MiddlewareOptions {
 		// console.log("Out of Constructor");
 	}
 
-	private async setStatusCode() {
-		if (this.testingStrategy === TestingStrategy.MANUAL) {
-			// this.statusCode = statusCode;
-			this.statusText = "Status Message here";
-			// console.log("Inside the if condition");
-		} else if (this.testingStrategy === TestingStrategy.RANDOM) {
-			await this.getStatusCode().then((res) => {
-				this.statusCode = res;
-			});
-			this.statusText = "Status Message here";
-		}
-		/* else {
+	private async setStatusCode(): Promise<any> {
+		try {
+			if (this.testingStrategy === TestingStrategy.MANUAL) {
+				// this.statusCode = statusCode;
+				this.statusText = "Status Message here";
+				// console.log("Inside the if condition");
+			} else if (this.testingStrategy === TestingStrategy.RANDOM) {
+				await this.getStatusCode().then((res) => {
+					this.statusCode = res;
+				});
+				this.statusText = "Status Message here";
+			}
+			/* else {
 
-        }
-        */
+            }
+            */
+			// console.log(this.statusCode, this.apiMethod);
+			return this.createResponse();
+		} catch (error) {
+			throw error;
+		}
+	}
+
+	private async createResponse(): Promise<any> {
+		try {
+			let statusCodeKey: string;
+			let responseBody: string;
+			let responseHeader: string;
+
+			// console.log(this.apiMethod, this.apiURL);
+			// console.log(statusCode, "statusCode");
+			// console.log(context.options.method);
+
+			statusCodeKey = (await this.statusCode) === 429 ? "429" : `${Math.floor(this.statusCode / 100)}xx`;
+			responseHeader = this.responseMap.get(statusCodeKey).get("responseHeader");
+			responseBody = this.responseMap.get(statusCodeKey).get("responseBody");
+
+			// console.log(responseHeader);
+			// console.log(JSON.parse(responseBody));
+
+			// responseBody = this.responseMap[statusCodeKey].responseBody;
+
+			const init = { url: this.apiURL, status: this.statusCode, statusText: this.statusText, header: JSON.parse(responseHeader) };
+			const response = new Response(JSON.parse(responseBody), init);
+			// console.log(response);
+			return response;
+		} catch (error) {
+			throw error;
+		}
 	}
 
 	private async getStatusCode(): Promise<number> {
 		try {
-			const fs = require("fs");
+			// const fs = require("fs");
 			/* let methodStatusCode;
         methodStatusCode = data.methodStatusCode;
         */
@@ -92,9 +278,10 @@ export class TestingHandlerOptions implements MiddlewareOptions {
 		}
 	}
 
-	public async sendMethod(apiMethod: string) {
+	public async sendMethod(apiMethod: string, url: string): Promise<any> {
 		// console.log(apiMethod, "hey");
 		this.apiMethod = apiMethod;
-		await this.setStatusCode();
+		this.apiURL = url;
+		return this.setStatusCode();
 	}
 }
