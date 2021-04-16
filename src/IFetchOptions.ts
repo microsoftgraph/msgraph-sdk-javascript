500697c025fe3a42fefe3f0052ade88d0f0d89e5
Nikitha Chettiar <nikithauc@gmail.com>
2021-04-16 15:13:03 -0700

    Update node-fetch option

--- src/IFetchOptions.ts
+++ src/IFetchOptions.ts
@@ -8,18 +8,20 @@
 /**
  * @interface {@link https://github.com/bitinn/node-fetch/#options}
  * Signature to define the fetch request options for node environment
- * @property {number} [follow] - Maximum redirect count. 0 to not follow redirect
- * @property {number} [timeout] - Request/Response timeout in milliseconds, it resets on redirect. 0 to disable (OS limit applies)
- * @property {number} [compress] - Support gzip/deflate content encoding. false to disable
- * @property {number} [size] - Maximum response body size in bytes. 0 to disable
- * @property {any} [agent] - HTTP(S).Agent instance, allows custom proxy, certificate, lookup, family etc.
+ * @property {number} [follow] - node-fetch option: maximum redirect count. 0 to not follow redirect
+ * @property {number} [compress] - node-fetch option: support gzip/deflate content encoding. false to disable
+ * @property {number} [size] - node-fetch option: maximum response body size in bytes. 0 to disable
+ * @property {any} [agent] - node-fetch option: HTTP(S).Agent instance, allows custom proxy, certificate, lookup, family etc.
+ * @property {number} [highWaterMark] - node-fetch option: maximum number of bytes to store in the internal buffer before ceasing to read from the underlying resource.
+ * @property {boolean} [insecureHTTPParser] - node-fetch option: HTTP(S).Use an insecure HTTP parser that accepts invalid HTTP headers when `true`.
  */
 export interface NodeFetchInit {
 	follow?: number;
-	timeout?: number;
 	compress?: boolean;
 	size?: number;
 	agent?: any;
+	highWaterMark?: number;
+	insecureHTTPParser?: boolean;
 }
 
 /**
