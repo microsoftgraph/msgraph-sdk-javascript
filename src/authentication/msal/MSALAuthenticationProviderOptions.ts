a83b761658d067a8641ebfeb7f30e64d1f771cf3
Nikitha Chettiar <nikithauc@gmail.com>
2021-07-13 20:10:53 -0700

    creating msal browser auth provider

--- src/authentication/msal/MSALAuthenticationProviderOptions.ts
+++ src/authentication/msal/MSALAuthenticationProviderOptions.ts
@@ -9,6 +9,8 @@
  * @module MSALAuthenticationProviderOptions
  */
 
+import { AccountInfo } from "@azure/msal-browser";
+
 import { AuthenticationProviderOptions } from "../../IAuthenticationProviderOptions";
 
 /**
@@ -33,4 +35,26 @@ export class MSALAuthenticationProviderOptions implements AuthenticationProvider
 	public constructor(scopes: string[]) {
 		this.scopes = scopes;
 	}
+}
+
+/**
+ * LoginType
+ *
+ * @export
+ * @enum {number}
+ */
+export enum LoginType {
+	/**
+	 * Popup = 0
+	 */
+	Popup,
+	/**
+	 * Redirect = 1
+	 */
+	Redirect,
+}
+
+export interface MSALBrowserAuthenticationProviderOptions extends AuthenticationProviderOptions {
+	loginType?: LoginType;
+	getAccount?: () => AccountInfo;
 }
