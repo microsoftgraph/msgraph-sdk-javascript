ce70d46c7b781c58c70b91d64715aac6ad84faa7
Nikitha Chettiar <nikithauc@gmail.com>
2022-02-08 10:18:46 -0800

    remove dom-shim

--- dom-shim.d.ts
+++
@@ -1,7 +0,0 @@
-interface Buffer {}
-interface NodeStream{
-    readable: boolean;
-         readableLength: number;
-        read(size?: number): any;
-       on(event: string | symbol, listener: (...args: any[]) => void): this;
-}
