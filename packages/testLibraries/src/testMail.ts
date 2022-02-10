43eaa610a05ce18c3e44f54a2d6f4e36b118d6db
Nikitha Chettiar <nikithauc@gmail.com>
2022-02-10 00:13:22 -0800

    testmail update

--- packages/testLibraries/src/testMail.ts
+++ packages/testLibraries/src/testMail.ts
@@ -1,5 +1,5 @@
-import {GraphServiceClient} from "graph-service-library";
-import {SimpleAuthenticationProvider} from "@microsoft/microsoft-graph-client";
+import { GraphServiceClient } from "graph-service-library";
+import { SimpleAuthenticationProvider } from "@microsoft/microsoft-graph-client";
 
 
 
@@ -7,17 +7,17 @@ import {SimpleAuthenticationProvider} from "@microsoft/microsoft-graph-client";
 
 const token = "ACCESS_TOKEN";
 
-const authProvider = new SimpleAuthenticationProvider(async() => {return token;});
-const serviceclient = GraphServiceClient.init({authProvider});
+const authProvider = new SimpleAuthenticationProvider(async () => { return token; });
+const serviceclient = GraphServiceClient.init({ authProvider });
 
 
-async function  test() {
-const messages = await serviceclient.usersById("813956a3-4a30-4596-914f-bfd86a657a09").messages.get();
+async function test() {
+    const messages = await serviceclient.usersById("813956a3-4a30-4596-914f-bfd86a657a09").messages.get();
 
-console.log(messages);
+    console.log(messages);
 
-const coreMe = await serviceclient.api("/me").get();
-console.log(coreMe);
+    const coreMe = await serviceclient.api("/me").get();
+    console.log(coreMe);
 }
 
 test().catch(console.error);
