d2c7927d2feb7cf9f64913b6bf3ae930ee20a221
Nikitha Chettiar <nikithauc@gmail.com>
2023-01-06 13:47:49 -0800

    Rollup and rollup-terser bump dependency version  (#1129)
    * rollup and terser update
    
    * Update ci_validation.yml
    
    * terser 0.3.0
    
    * recreate lock json

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@
  * -------------------------------------------------------------------------------------------
  */
 
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
