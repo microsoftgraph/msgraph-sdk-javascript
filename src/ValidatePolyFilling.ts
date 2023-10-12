5c7861da46f8fadac7d1868f26c2446ae7dfa13d
Dave Nicolson <david.nicolson@gmail.com>
2023-10-12 19:10:05 +0200

    Fix polyfill error grammar

--- src/ValidatePolyFilling.ts
+++ src/ValidatePolyFilling.ts
@@ -14,15 +14,15 @@
 
 export const validatePolyfilling = (): boolean => {
 	if (typeof Promise === "undefined" && typeof fetch === "undefined") {
-		const error = new Error("Library cannot function without Promise and fetch. So, please provide polyfill for them.");
+		const error = new Error("Library cannot function without Promise and fetch. So, please provide a polyfill for them.");
 		error.name = "PolyfillNotAvailable";
 		throw error;
 	} else if (typeof Promise === "undefined") {
-		const error = new Error("Library cannot function without Promise. So, please provide polyfill for it.");
+		const error = new Error("Library cannot function without Promise. So, please provide a polyfill for it.");
 		error.name = "PolyfillNotAvailable";
 		throw error;
 	} else if (typeof fetch === "undefined") {
-		const error = new Error("Library cannot function without fetch. So, please provide polyfill for it.");
+		const error = new Error("Library cannot function without fetch. So, please provide a polyfill for it.");
 		error.name = "PolyfillNotAvailable";
 		throw error;
 	}
